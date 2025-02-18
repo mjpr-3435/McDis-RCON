ea7997f60c1c1a9b150c68a4f4167702b625d3ce
Kassius <150753762+mjpr-3435@users.noreply.github.com>
2025-02-17 19:31:51 -0500

    Release McDis-RCON 0.4.15a

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="mcdis_rcon",
-    version="0.4.14a",
+    version="0.4.15a",
     packages=find_packages(),
     include_package_data=True,
     package_data={
