61280eaa5c8ef1f69aac4335f027b179e2fd96a2
Kassius <150753762+mjpr-3435@users.noreply.github.com>
2025-03-16 19:14:32 -0500

    fix: get_uuid

--- mcdis_rcon/utils/mc.py
+++ mcdis_rcon/utils/mc.py
@@ -6,7 +6,7 @@ def mc_uuid(player: str, *, online : bool = True):
 
         if response.status_code == 200:
             data = response.json()
-            return uuid.UUID(data['id'])
+            return uuid.UUID(data['id']).__str__()
     else:
         hash = hashlib.md5(f"OfflinePlayer:{player}".encode('utf-8')).digest()
         byte_array = [byte for byte in hash]
@@ -14,7 +14,7 @@ def mc_uuid(player: str, *, online : bool = True):
         byte_array[8] = hash[8] & 0x3f | 0x80
 
         hash_modified = bytes(byte_array)
-        offline_player_uuid = uuid.UUID(hash_modified.hex())
+        offline_player_uuid = uuid.UUID(hash_modified.hex()).__str__()
 
         return offline_player_uuid
 
