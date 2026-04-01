740d5bf9c961b60017a18f08fbe64216e22e66ea
Elí Maciel <tecno_eli@hotmail.com>
2026-04-01 01:23:05 -0600

    add Server#send_response

--- mcdis_rcon/classes/Server.py
+++ mcdis_rcon/classes/Server.py
@@ -7,3 +7,11 @@ from .McDisClient import McDisClient
 class Server(Process):
     def __init__(self, name: str, client: McDisClient, config: dict):
         super().__init__(name, client, config)
+        
+    def send_response(self, target: str, message: str | list[str], *, colour: str = "gray"):
+        if isinstance(message, str):
+            self.execute(f'tellraw {target} {{"text": "{message}","color":"{colour}"}}')
+
+        elif isinstance(message, list) and all(isinstance(i, str) for i in message):
+            for msg in message:
+                self.execute(f'tellraw {target} {{"text": "{msg}","color":"{colour}"}}')
